c62ef3e4db7629a53982a3934bab72f8cca2a282
Jichani <123962013+jichani@users.noreply.github.com>
2024-09-04 23:17:09 +0900

    컴포넌트 및 모듈 시스템 공부

---
+++ 동물 앨범 만들기-1/src/App.js
@@ -0,0 +1,7 @@
+import Content from "./components/Content";
+import TabBar from "./components/TabBar";
+
+export default function App($app) {
+  const tabBar = new TabBar();
+  const content = new Content();
+}
