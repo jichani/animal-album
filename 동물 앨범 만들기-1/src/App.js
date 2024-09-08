50cd8cd644f304a010e188d1c29971ea15a124c3
Jichani <123962013+jichani@users.noreply.github.com>
2024-09-08 19:29:32 +0900

    수정

--- 동물 앨범 만들기-1/src/App.js
+++ 동물 앨범 만들기-1/src/App.js
@@ -1,7 +1,42 @@
+import { request } from "./components/api";
 import Content from "./components/Content";
 import TabBar from "./components/TabBar";
 
 export default function App($app) {
-  const tabBar = new TabBar();
+  this.state = {
+    currentTab: "all",
+    photos: [],
+  };
+
+  const tabBar = new TabBar({
+    $app,
+    initialState: "",
+    onClick: async name => {
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos: await request(name),
+      });
+    },
+  });
+
   const content = new Content();
+
+  this.setState = newState => {
+    this.state = newState;
+    tabBar.setState(this.state.currentTab);
+    content.setState(this.state.photos);
+  };
+
+  const init = async () => {
+    try {
+      const initialPhotos = await request();
+      this.setState({
+        ...this.state,
+        photos: initialPhotos,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 }
